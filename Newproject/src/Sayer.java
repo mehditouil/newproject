d396572f31e44dedfd06a0253c678a1777d09211
mehditouil <mehdi.touil@gmail.com>
2016-12-04 21:38:16 +0100

    this is a commit

---
+++ Newproject/src/Sayer.java
@@ -0,0 +1,17 @@
+public class Sayer {
+ int number = 0;
+
+public int getNumber() {
+	return number;
+}
+//this is a method like named setter
+public void setNumber(int number) {
+	this.number = number;
+}
+
+public Sayer(int number) {
+	super();
+	this.number = number;
+}
+ 
+}
